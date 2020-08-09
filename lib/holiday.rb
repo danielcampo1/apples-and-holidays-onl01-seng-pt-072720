76023bbd80b10f1c73f9b5ed1d068220d3d40819
daniel campo <campo_daniel94@yahoo.com>
2020-08-09 19:35:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  holiday_hash[:spring][:memorial_day]
+  holiday_hash[:spring][:memorial_day][0]
   # return the second element in the 4th of July array
  
 end
